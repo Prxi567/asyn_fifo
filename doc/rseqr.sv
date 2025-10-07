24b23e6d62b5ce678e9552c4a37b5f12e3b9011a
Prxi567 <168269781+Prxi567@users.noreply.github.com>
2025-10-07 23:16:07 +0530

    Update rseqr.sv

--- doc/rseqr.sv
+++ doc/rseqr.sv
@@ -1,1 +1,8 @@
-
+class rseqr extends uvm_sequencer #(rseq_item);
+  
+  `uvm_component_utils(rseqr)
+  
+  function new(string name = "rseqr",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+endclass
